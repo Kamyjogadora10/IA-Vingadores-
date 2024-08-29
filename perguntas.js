0ac4e57145ddcafc1e1c714a3cd3987da757fccc
Kamyjogadora10 <117648068+Kamyjogadora10@users.noreply.github.com>
2024-08-29 20:23:32 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,135 @@
+export const perguntas = [
+    {
+        enunciado: "1. Quais são seus hobbies ou atividades favoritas para passar o tempo?",
+        alternativas: [
+            {
+                texto: "A) Atividades que envolvam o racíocinio lógico, leitura e jogos",
+                afirmacao: [
+                    " O Homem de Ferro. Você é alguém carismático e complexo. Aonde se destaca por sua confiança e charme, como um gênio inventor, tipo o Tony Stark, mas sua autoconfiança pode se tornar arrogância e vaidade. Apesar de seus defeitos, você é altruísta e responsável, sempre usando suas habilidades para o bem maior."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Atividades físicas, que envolva treinamento, resistencia e luta ",
+                afirmacao: [
+                    "O Capitão América. Você é conhecido por sua integridade e moral inabalável. Aonde personifica a ética e a moralidade, sempre defendendo seus princípios com coragem e determinação, mesmo frente a grandes desafios. Sua lealdade aos amigos e aos valores que acredita é profunda, isso reflete seu compromisso com algo. Buscando sempre o que é certo."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Você se considera :",
+        alternativas: [
+            {
+                texto: "Uma pessoa muito autoconfiante, com charme e super habilidoso naquilo que faz bem",
+                afirmacao: [
+                    "O Homem de Ferro. Você é alguém carismático e complexo. Aonde se destaca por sua confiança e charme, como um gênio inventor, tipo o Tony Stark, mas sua autoconfiança pode se tornar arrogância e vaidade. Apesar de seus defeitos, você é altruísta e responsável, sempre usando suas habilidades para o bem maior.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Uma pessoa introvertida, humilde e bastante responsável com as pessoas",
+                afirmacao: [
+                    "O Capitão América. Você é conhecido por sua integridade e moral inabalável. Aonde personifica a ética e a moralidade, sempre defendendo seus princípios com coragem e determinação, mesmo frente a grandes desafios. Sua lealdade aos amigos e aos valores que acredita é profunda, isso reflete seu compromisso com algo. Buscando sempre o que é certo."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você se identifica mais com a liderança ou mentalidade de qual vingador? ",
+        alternativas: [
+            {
+                texto: "Com a mentalidade inovadora do Homem de Ferro e suas ideias mirabolantes ",
+                afirmacao: [
+                    "O Homem de Ferro. Você é alguém carismático e complexo. Aonde se destaca por sua confiança e charme, como um gênio inventor, tipo o Tony Stark, mas sua autoconfiança pode se tornar arrogância e vaidade. Apesar de seus defeitos, você é altruísta e responsável, sempre usando suas habilidades para o bem maior."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Com a liderança do Capitão América e sua busca por justiça ",
+                afirmacao: [
+                    " O Capitão América. Você é conhecido por sua integridade e moral inabalável. Aonde personifica a ética e a moralidade, sempre defendendo seus princípios com coragem e determinação, mesmo frente a grandes desafios. Sua lealdade aos amigos e aos valores que acredita é profunda, isso reflete seu compromisso com algo. Buscando sempre o que é certo."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: " Qual é o seu cenário ou local preferido em um filme dos Vingadores",
+        alternativas: [
+            {
+                texto: " Vingadores: Ultimato",
+                afirmacao: [
+                    "A Viúva Negra. Você se destaca por sua força interior, frieza e eficiência em situações extremas, Inteligente e estratégica, é uma líder excepcional em tudo o que faz.  Apesar de sua fachada dura, carrega uma bagagem emocional pesada e um profundo senso de culpa, o que a motiva a buscar redenção e lutar por uma causa maior. Sempre demonstrando uma lealdade inabalável aos amigos, sacrificando-se para proteger os outros e lutar pelo que é justo."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Vingadores: Guerra Infinita",
+                afirmacao: [
+                    " O Capitão América. Você é conhecido por sua integridade e moral inabalável. Aonde personifica a ética e a moralidade, sempre defendendo seus princípios com coragem e determinação, mesmo frente a grandes desafios. Sua lealdade aos amigos e aos valores que acredita é profunda, isso reflete seu compromisso com algo. Buscando sempre o que é certo."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você pudesse ter um único poder, qual dos seguintes você escolheria:",
+        alternativas: [
+            {
+                texto: "Ter dinhero e poder político para fazer aquilo que quiser ou conseguir construir qualquer coisa",
+                afirmacao: [
+                    "O Homem de Ferro. Você é alguém carismático e complexo. Aonde se destaca por sua confiança e charme, como um gênio inventor, tipo o Tony Stark, mas sua autoconfiança pode se tornar arrogância e vaidade. Apesar de seus defeitos, você é altruísta e responsável, sempre usando suas habilidades para o bem maior."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Ter a escolha de ser modificado científicamente, se transformando em uma arma humana quase imortal",
+                afirmacao: [
+                    "A Viúva Negra. Você se destaca por sua força interior, frieza e eficiência em situações extremas, Inteligente e estratégica, é uma líder excepcional em tudo o que faz.  Apesar de sua fachada dura, carrega uma bagagem emocional pesada e um profundo senso de culpa, o que a motiva a buscar redenção e lutar por uma causa maior. Sempre demonstrando uma lealdade inabalável aos amigos, sacrificando-se para proteger os outros e lutar pelo que é justo."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual foi o melhor vilão de Vingadores?",
+        alternativas: [
+            {
+                texto: " O Thanos",
+                afirmacao: [
+                    "O Homem de Ferro. Você é alguém carismático e complexo. Aonde se destaca por sua confiança e charme, como um gênio inventor, tipo o Tony Stark, mas sua autoconfiança pode se tornar arrogância e vaidade. Apesar de seus defeitos, você é altruísta e responsável, sempre usando suas habilidades para o bem maior."
+                ],
+            },
+            {
+                texto: "O Loki",
+                afirmacao: [
+                    "O Thor. Você é uma figura emblemática, marcada por coragem, força e um profundo senso de honra. Que enfrenta desafios com bravura e lealdade, sempre pronto para proteger aqueles que ama. Independente se for aquela pessoa amada for um vilão ou não. Assim, refletindo um equilíbrio entre força e bondade. "
+                ],
+                proxima: 9,
+            },
+        
+        ]
+    },
+    {
+        enunciado: "Qual é a sua abordagem para resolver conflitos?",
+        alternativas: [
+            {
+                texto: "Você utiliza da habilidade de comunicação e persuasão para conseguir aquilo que quer",
+                afirmacao: [
+                    "A Viúva Negra. Você se destaca por sua força interior, frieza e eficiência em situações extremas, Inteligente e estratégica, é uma líder excepcional em tudo o que faz.  Apesar de sua fachada dura, carrega uma bagagem emocional pesada e um profundo senso de culpa, o que a motiva a buscar redenção e lutar por uma causa maior. Sempre demonstrando uma lealdade inabalável aos amigos, sacrificando-se para proteger os outros e lutar pelo que é justo."
+                ],
+            },
+            {
+                texto: "Você utiliza de força e resistência para alcançar seu objetivo",
+                afirmacao: [
+                    "O Thor. Você é uma figura emblemática, marcada por coragem, força e um profundo senso de honra. Que enfrenta desafios com bravura e lealdade, sempre pronto para proteger aqueles que ama. Independente se for aquela pessoa amada for um vilão ou não. Assim, refletindo um equilíbrio entre força e bondade. "
+                ],
+                proxima: 10,
+            },
+        ]
+    }
+];
